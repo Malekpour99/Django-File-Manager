b89ed860b3a24cffc6257f47d4f5d0980e99c3cf
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-28 15:28:26 +0330

    core URLs - REST framework & swagger URLs
    Added schema view with swagger URLs for API documentations and REST framework URLs to the project's core URLs

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,13 +14,42 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="File Manager API",
+        default_version="v1",
+        description="Test API for File Manager's File model",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="work.malekpour99@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path(
+        "swagger/api.json", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 if settings.DEBUG:
