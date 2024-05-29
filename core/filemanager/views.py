2174a9d49c02b604de06a424da02d95f80e7973a
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-29 22:17:51 +0330

    filemanager views - created file upload CBV
    Created file upload view as a Create CBV to manage file upload processes and redirecting after successful uploads

--- core/filemanager/views.py
+++ core/filemanager/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import File
 from accounts.models import Profile
 
@@ -18,3 +18,23 @@ class FileListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         files = File.objects.filter(owner__user__id=self.request.user.id)
         return files
+
+
+class FileUploadView(LoginRequiredMixin, CreateView):
+    """
+    Uploading a new file and dedicating this file to the current user
+    """
+
+    template_name = "filemanager/file-list.html"
+    model = File
+    fields = ["file"]
+    success_url = reverse_lazy("filemanager:home")
+
+    def form_valid(self, form):
+        form.instance.owner = Profile.objects.get(user__id=self.request.user.id)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["files"] = File.objects.filter(owner__user=self.request.user)
+        return context
