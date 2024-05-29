50dae9f383327b670a2ff31d11522d490c2a7d12
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-29 10:45:19 +0330

    filemanager admin - File & Folder models registration
    Registered File and Folder models in the Django admin panel

--- core/filemanager/admin.py
+++ core/filemanager/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import File, Folder
 
-# Register your models here.
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    # Activates post filtering by date hierarchy
+    date_hierarchy = "created_at"
+    empty_value_display = "-empty-"
+    list_display = (
+        "name",
+        "owner",
+        "size",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("created_at", "updated_at")
+    search_fields = ["name"]
+
+
+@admin.register(Folder)
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    empty_value_display = "-empty-"
+    list_display = (
+        "name",
+        "owner",
+        "parent_folder",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("parent_folder", "created_at", "updated_at")
+    search_fields = ["name", "parent_folder"]
