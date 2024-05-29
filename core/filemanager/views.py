2018c47670c29b7479cf249501cc3e2a63b8199f
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-29 13:08:36 +0330

    filemanager views - Created FileListView
    Created file list view as a ListView CBV to show authenticated owner uploaded files

--- core/filemanager/views.py
+++ core/filemanager/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.views.generic import ListView
+from .models import File
+from accounts.models import Profile
 
-# Create your views here.
+
+class FileListView(LoginRequiredMixin, ListView):
+    """
+    Showing list of files for the authenticated owner
+    """
+
+    template_name = "filemanager/file-list.html"
+    context_object_name = "files"
+
+    def get_queryset(self):
+        files = File.objects.filter(owner__user__id=self.request.user.id)
+        return files
