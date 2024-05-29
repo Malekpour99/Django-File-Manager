013f63b6596db533a9abe7e92487c6c9081a7e46
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-29 08:47:16 +0330

    core settings - filemanager app
    Registered filemanager app in the project's core INSTALLED_APPS

--- core/core/settings.py
+++ core/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "filemanager",
     "corsheaders",
     "rest_framework",
     "rest_framework.authtoken",
