b448629f749a814cbaecd20595011add746e5e36
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-29 22:18:39 +0330

    filemanager URLs - upload file URL
    Created a URL path for managing file uploads

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "filemanager"
+
 urlpatterns = [
     path("", views.FileListView.as_view(), name="home"),
+    path("file/upload/", views.FileUploadView.as_view(), name="upload-file"),
 ]
