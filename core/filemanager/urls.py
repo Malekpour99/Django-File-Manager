fe2933dc29a31fafa2066d67786351223f73df7b
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-30 19:02:12 +0330

    filemanager URLs - folder content URL
    Created a URL path for viewing folder contents which is mapped to the content view and will send the folder slug to that view

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -5,6 +5,7 @@ app_name = "filemanager"
 
 urlpatterns = [
     path("", views.ContentView.as_view(), name="home"),
+    path("<str:folder_slug>/", views.ContentView.as_view(), name="folder-content"),
     path("file/upload/", views.FileUploadView.as_view(), name="upload-file"),
     path("file/<int:pk>/delete/", views.FileDeleteView.as_view(), name="delete-file"),
     path("folder/<int:pk>/delete/", views.FolderDeleteView.as_view(), name="delete-folder"),
