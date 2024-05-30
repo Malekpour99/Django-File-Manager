15bc11432f8f43efcdba675856626fb92d5148cb
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-30 18:03:34 +0330

    filemanager URLs - deleter folder URL
    Created a URL for deleting folders which is mapped to the delete folder view

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.ContentView.as_view(), name="home"),
     path("file/upload/", views.FileUploadView.as_view(), name="upload-file"),
     path("file/<int:pk>/delete/", views.FileDeleteView.as_view(), name="delete-file"),
+    path("folder/<int:pk>/delete/", views.FolderDeleteView.as_view(), name="delete-folder"),
 ]
