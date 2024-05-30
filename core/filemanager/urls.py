3ff12b02200ff5d4f724c30bdbaf8751c07da4c5
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-30 09:01:24 +0330

    filemanager URLs - delete file URL
    Created a URL for deleting files and mapped it to its relevant view

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -6,4 +6,5 @@ app_name = "filemanager"
 urlpatterns = [
     path("", views.FileListView.as_view(), name="home"),
     path("file/upload/", views.FileUploadView.as_view(), name="upload-file"),
+    path("file/<int:pk>/delete/", views.FileDeleteView.as_view(), name="delete-file"),
 ]
