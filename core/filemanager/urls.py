52631546f4e85a617f48acc1372f1c45039ca716
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-30 14:27:11 +0330

    filemanager URLs - home URL view
    Updated home URL view from FileListView to ContentView

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "filemanager"
 
 urlpatterns = [
-    path("", views.FileListView.as_view(), name="home"),
+    path("", views.ContentView.as_view(), name="home"),
     path("file/upload/", views.FileUploadView.as_view(), name="upload-file"),
     path("file/<int:pk>/delete/", views.FileDeleteView.as_view(), name="delete-file"),
 ]
