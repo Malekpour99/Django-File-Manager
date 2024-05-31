95819f505b2034c7a15731981deb9ebe7e557d71
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-31 07:31:18 +0330

    filemanager URLs - create-folder URL + update
    Created a URL path for creating new folders and updated the upload-file URL path

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -6,7 +6,8 @@ app_name = "filemanager"
 urlpatterns = [
     path("", views.ContentView.as_view(), name="home"),
     path("folder/<str:folder_slug>/", views.ContentView.as_view(), name="folder-content"),
-    path("file/upload/", views.FileUploadView.as_view(), name="upload-file"),
+    path("upload/file/", views.FileUploadView.as_view(), name="upload-file"),
+    path("create/folder/", views.FolderCreateView.as_view(), name="create-folder"),
     path("file/<int:pk>/delete/", views.FileDeleteView.as_view(), name="delete-file"),
     path("folder/<int:pk>/delete/", views.FolderDeleteView.as_view(), name="delete-folder"),
 ]
