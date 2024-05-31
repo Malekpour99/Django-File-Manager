d947c40c29ec18e52f11fa83b454c0704e28adef
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-31 22:37:02 +0330

    filemanager URLs - update-file URL
    Created a URL path for updating file's name which is mapped to the FileUpdateView

--- core/filemanager/urls.py
+++ core/filemanager/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("folder/<str:folder_slug>/", views.ContentView.as_view(), name="folder-content"),
     path("upload/file/", views.FileUploadView.as_view(), name="upload-file"),
     path("create/folder/", views.FolderCreateView.as_view(), name="create-folder"),
+    path("file/<int:pk>/edit/", views.FileUpdateView.as_view(), name="update-file"),
     path("file/<int:pk>/delete/", views.FileDeleteView.as_view(), name="delete-file"),
     path("folder/<int:pk>/delete/", views.FolderDeleteView.as_view(), name="delete-folder"),
     path("search/", views.SearchView.as_view(), name="search"),
